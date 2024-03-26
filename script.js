d2d63c8acd6ced57b6e1f983f864dd5dcb254271
LcsGa <lcs.grc@outlook.com>
2024-03-26 11:01:53 +0100

    implement the poc

---
+++ script.js
@@ -0,0 +1,66 @@
+const phoneInput = document.querySelector("input[type=tel]");
+const addPhoneButton = document.querySelector(".phone-field button");
+
+const phonesEl = document.querySelector(".phones");
+
+const message = document.querySelector("textarea");
+const sendButton = document.querySelector(".message button");
+
+let phones = [];
+
+onload = () => {
+  phoneInput.focus();
+};
+
+phoneInput.addEventListener("keydown", ({ key }) => {
+  if (key === "Enter") {
+    addPhoneButton.click();
+  }
+});
+
+addPhoneButton.addEventListener("click", () => {
+  const phone = phoneInput.value.trim();
+  if (/^\+?\d{4,}$/.test(phone)) {
+    phones.push(phoneInput.value);
+    renderPhones();
+  }
+  phoneInput.value = "";
+});
+
+sendButton.addEventListener("click", () => {
+  const msg = message.value.trim();
+
+  if (phones.length && msg) {
+    const newTab = open();
+
+    console.log(newTab.location);
+
+    newTab.location.replace(`sms:/open?addresses=${phones.join(",")}&body=${msg}`);
+
+    console.log(newTab.location);
+  }
+});
+
+function renderPhones() {
+  phonesEl.innerHTML = "";
+
+  phones.forEach((phone, i) => {
+    const phoneEl = document.createElement("div");
+    phoneEl.classList.add("chip", "phone");
+
+    const phoneDisplay = document.createElement("p");
+    phoneDisplay.innerText = phone;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "x";
+
+    phoneEl.append(phoneDisplay, deleteButton);
+
+    phoneEl.addEventListener("click", () => {
+      phones = phones.filter((_, _i) => _i !== i);
+      renderPhones();
+    });
+
+    phonesEl.appendChild(phoneEl);
+  });
+}
